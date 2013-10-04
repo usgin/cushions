9009d1f69d445de45dfb7bce71c58592a6eebce3
Ryan Clark <ryan.clark.j@gmail.com>
2013-10-04 14:15:59 -0700

    refactored correction routines, use async

--- bin/cushions.js
+++ bin/cushions.js
@@ -5,143 +5,90 @@ var io = require('socket.io').listen(3000),
     async = require('async');
 
 io.sockets.on('connection', function (socket) {
-
-  socket.on('autocorrect', function (dbName, corrections) {
-    autoCorrect(socket, dbName, corrections);
+  socket.on('autocorrect', function (dbName) {
+    Corrector(socket, dbName).autoFix();
   });
-  
 });
 
-function doCorrections(socket, db, corrections) {
-  corrections = corrections || {};
-  var ids = _.keys(corrections);
+function Corrector(socket, dbName) {
+  var db = nano.use(dbName),
+      corrections = {},
+      recordsFixed = 0;
   
-  socket.emit('fixingRecords', ids.length);
-  
-  
-  
-  async.each(ids, iterator, finished)
-  
-  
-  function iterator(id, callback) {
-    db.get(id, gotDoc);
-    
-    function gotDoc(err, doc) {
-      if (err) { 
-        socket.emit('error', err); 
-        callback(err); 
-      } else {
-        _.extend(doc, corrections[id]);
-        db.insert(doc, doc._id, updatedDoc);
-      }
-    }
-    
-    function updatedDoc(err, response) {
-      if (!err && i % 100 === 0) { socket.emit('fixedRecord', i); }
-      else if (err) { socket.emit('error', err); }
-      callback(err);
-    }
-  }
-  
-  _.forOwn(corrections, function (id) { db.get(id, gotDoc); });
-  
-  
-  
-  function updatedDoc(err, response) {
-    if (!err && i % 100 === 0) { socket.emit('fixedRecord', i); }
-    else if (err) { socket.emit('error', err); }
-    isFinished();
-  }
-}
-
-function gatherCorrections(socket, db) {
-  var corrections = {};
-  
-  db.get('_design/validation', gotDesignDoc);
-  
-  function gotDesignDoc(err, design) {
-    if (err) { socket.emit('error', err); return; }
-    
-    var criteria = _.keys(design.criteria),
-        i = 0, finished = criteria.length;
-    
-    socket.emit('gatheringSuggestions', finished);
-    
-    isFinished(false);
-    
-    function isFinished(step) {
-      step = step || true;
-      if (step) { i++; }
-      if (i === finished  || finished === 0) { 
-        socket.emit('gatheredSuggestions', corrections); 
-      }
-    }
-    
-    
-    
-    
-    
-  }
-}
-
-function autoCorrect(socket, dbName, corrections) {
-  var db = nano.use(dbName);
-  corrections = corrections || {};
-  
-  if (_.isEmpty(corrections)) {
-    // If no suggestions were passed in, assume we should correct all
-    
-  }
-  
-  else {
-    // Was given some corrections, so just run those
-    doCorrections(socket, db, corrections);
-  }
-}
-/*
-    db.get('_design/validation', function (err, design) {
-      if (err) { socket.emit('error', err); return; }
+  function eachView(viewName, callback) {
+    db.view('validation', viewName, function (err, response) {
+      if (err) { callback(err); return; }
       
-      var getSuggestions = function () {
-        if (criteria.length > 0) {
-          getSuggestionSet(criteria.pop(), function (err) {
-            if (err) { socket.emit('error', err); return; }
-            socket.emit('finishedCriteria');
-            getSuggestions();
-          });
-        } else {
-          socket.emit('gatheredSuggestions');
-          doCorrections();
-        } 
-      };
+      var corrections = _.map(response.rows, function (row) {
+        return {id: row.id, suggestion: row.value.suggestion};
+      });
       
-      var getSuggestionSet = function (viewName, callback) {
-        db.view('validation', viewName, function (err, response) {
-          if (err) { callback(err); return; }
-          
-          if (_.some(response.rows, function (row) { return row.value.hasOwnProperty('suggestion'); })) {
-            _.each(response.rows, function (row) {
-              if (_.contains(_.keys(suggestions), row.id)) {
-                _.extend(suggestions[row.id], row.value.suggestion);  
-              } else {
-                suggestions[row.id] = row.value.suggestion;
-              } 
-            });
-          }
-          
-          callback(null, null);
-        });
-      };
+      corrections = _.filter(corrections, function (correction) {
+        return correction.suggestion;  
+      });
       
-      var criteria = _.keys(design.criteria);
-      socket.emit('gatheringSuggestions', criteria.length);
-      getSuggestions();
-      
+      callback(null, corrections);
+      socket.emit('finishedCriteria');
     });
   }
-  else {
-    // We recieved some suggestions
-    doCorrections();
+  
+  function fixOne(id, callback) {
+    var corrections = corrections[id] || {};
+    
+    db.get(id, function (err, doc) {
+      if (err) { callback(err); return; }
+      
+      _.extend(doc, corrections);
+      db.insert(doc, id, function (err, response) {
+        recordsFixed++;
+        if (!err && recordsFixed % 100 === 0) { socket.emit('fixedRecord', recordsFixed); }
+        else if (err) { socket.emit('error', err); }
+        callback(err);
+      });
+    });
   }
   
+  var corrector = {
+    autoFix: function () {
+      this.gatherCorrections(function (err, corrections) {
+        if (err) { socket.emit('error', err); return; }
+        corrector.fix();
+      });
+    },
+    
+    gatherCorrections: function (callback) {
+      db.get('_design/validation', function (err, design) {
+        if (err) { callback(err); return; }  
+        
+        var viewNames = _.keys(design.criteria);
+        socket.emit('gatheringSuggestions', viewNames.length);
+        async.map(viewNames, eachView, function (err, results) {
+          _.each(results, function (criteriaResult) {
+            _.each(criteriaResult, function (result) {
+              if (_.has(corrections, result.id)) {
+                _.extend(corrections[result.id], result.suggestion);
+              } else {
+                corrections[result.id] = result.suggestion;
+              }
+            });
+          });
+          
+          callback(err, corrections);
+          socket.emit('gatheredSuggestions');
+        });
+      });
+    },
+    
+    fix: function () {
+      var ids = _.keys(corrections);
+      
+      socket.emit('fixingRecords', ids.length);
+      async.each(ids, fixOne, function () {
+        socket.emit('allFixed');
+        recordsFixed = 0;
+      });
+    }
+  };
+  
+  return corrector; 
 }
